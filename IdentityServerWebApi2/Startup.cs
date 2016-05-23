4ecf0c8f53ad55b0f85a72588c7714e5d90a6784
Mahmut Gündoğdu <mahmutgundogdu@hotmail.com.tr>
2016-05-23 14:47:27 +0300

    all example in one project

---
+++ IdentityServerWebApi2/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using IdentityServer3.AccessTokenValidation;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(IdentityServerWebApi2.Startup))]
+
+namespace IdentityServerWebApi2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = "http://localhost:5000",
+                ValidationMode = ValidationMode.ValidationEndpoint,
+
+                RequiredScopes = new[] { "api1" }
+            });
+
+            // configure web api
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            // require authentication for all controllers
+            config.Filters.Add(new AuthorizeAttribute());
+
+            app.UseWebApi(config);
+        }
+    }
+}
---
+++ console2/Program.cs
@@ -0,0 +1,39 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var token = GetClientToken();
+            CallApi(token);
+            Console.ReadLine();
+        }
+
+
+        static TokenResponse GetClientToken()
+        {
+            var client = new TokenClient(
+                "http://localhost:5000/connect/token",
+                "silicon",
+                "F621F470-9731-4A25-80EF-67A6F7C5F4B8");
+
+            return client.RequestClientCredentialsAsync("api1").Result;
+        }
+
+        static void CallApi(TokenResponse response)
+        {
+            var client = new HttpClient();
+            client.SetBearerToken(response.AccessToken);
+
+            Console.WriteLine(client.GetStringAsync("http://localhost:53118/test").Result);
+        }
+    }
+}
